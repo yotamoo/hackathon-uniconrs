3a48bcc02c7ae0b078a6931438358d487c60442a
Yotam Ohayon <ohayon.yotam@gmail.com>
2023-04-22 13:23:46 +0200

    some bug fixes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.post('/verify', (req, res) => {
   // 1. verify sender is not imporsonated, use public key
   // 2. verify sender is `ambassadors` or `verified`
   const sender = req.body.sender
-  const isSenderValid = ambassadors.includes(sender) || trusted.includes(sender)
+  const isSenderValid = ambassadors.includes(sender) || sender in trusted
 
   if (!isSenderValid) {
     console.log('Sender is not trusted')
@@ -28,12 +28,14 @@ app.post('/verify', (req, res) => {
   // 3. send candidate a verification email, use a uuid to make sure the right link is tapped
   const candidate = req.body.candidate
   if (candidate in trusted) {
+    // 3.1 check for double verifications
     trusted[candidate] = trusted[candidate] + 1
     res.status(200)
     res.send(`increased person's points to ${trusted[candidate]}`)
   } else {
     const uuid = '1123-3453-2342-4564-2341'
-    candidates[candidate] = { uuid, points: minimumToBeTrusted }
+    const points = ambassadors.includes(sender) ? minimumToBeTrusted : 1
+    candidates[candidate] = { uuid, points }
     // send email
     res.status(200)
     res.send(`Waiting for candidate\'s approval ${uuid}`)
@@ -44,7 +46,7 @@ app.post('/verify', (req, res) => {
 app.post('/join', (req, res) => {
   const sender = req.body.sender
   const uuid = req.body.uuid
-  const isSenderValid = candidates[sender].uuid == uuid
+  const isSenderValid = candidates[sender] !== undefined && candidates[sender].uuid == uuid
 
   if (!isSenderValid) {
     console.log('Sender is not a valid candidate')
@@ -59,17 +61,13 @@ app.post('/join', (req, res) => {
   res.send('Candidate added')
 })
 
-app.get('/network', (req, res) => {
-  var filtered = {}
-  Object.keys(trusted).forEach(key => {
-    if (trusted[key] >= minimumToBeTrusted) {
-      filtered[key] = trusted[key]
-    }
-  })
-  
-console.log('filtered', filtered);
-
-  res.send(filtered)
+app.post('/user', (req, res) => {
+  const score = trusted[req.body.email]
+  if (score === undefined ||score < minimumToBeTrusted) {
+    res.status(404).send(`${req.body.email} is not a trusted person`)
+  } else {
+    res.status(200).send({score})
+  }
 })
 
 app.listen(port, () => {
